e24ce5d52ed7a7dc764006e3b280db1c8491f56a
chiru381 <chirukosanam123@gmail.com>
2025-01-26 18:28:29 +0530

    all model files

---
+++ backend/models/Contact.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type:String,
+    //   required:true,
+    //   unique:true
+    },
+    email: {
+      type:String,
+    //   required:true,
+    //   unique:true
+    },
+    message: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Contact",ContactSchema,"Contact");
