5f9cdc32a0e915ec43f209339dbc7b09aff1b8b2
chiru381 <chirukosanam123@gmail.com>
2025-01-26 19:04:38 +0530

    backend index file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-    res.send("server connected");
-});
+const coverpageRoute = require("./routes/coverpages");
+const mottoRoute = require("./routes/motto");
+const projectRoute = require("./routes/projects");
+const certificateRoute = require("./routes/certificates");
+const skillsRoute = require("./routes/skills");
+const hobbyRoute = require("./routes/hobbies");
+const blogRoute = require("./routes/blogs");
+const contactRoute = require("./routes/contact");
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`running on port ${PORT}`);
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/portfolioooo',{
+    dbName: process.env.DB_NAME
+}).
+then(() => console.log('Database Connected')).catch(err => console.log(err));
+
+app.use(cors());
+
+app.use("/api/coverpages",coverpageRoute);    //no need
+app.use("/api/motto",mottoRoute);
+app.use("/api/projects",projectRoute);
+app.use("/api/certificates",certificateRoute);
+app.use("/api/skills", skillsRoute);
+app.use("/api/hobbies", hobbyRoute);        //no need
+app.use("/api/blogs", blogRoute);
+app.use("/api/contact", contactRoute);
+
+app.listen("5000",() => {
+    console.log("Backend is running.");
 });
