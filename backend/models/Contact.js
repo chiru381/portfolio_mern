df7951328933df15464b88abeabc6050e0492174
chiru381 <chirukosanam123@gmail.com>
2025-02-07 18:08:53 +0530

    updated

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -4,28 +4,26 @@ const ContactSchema = new mongoose.Schema(
   {
     name: {
       type:String,
-    //   required:true,
-    //   unique:true
+      required:true,
+      unique:true
     },
     email: {
       type:String,
-    //   required:true,
-    //   unique:true
+      required:true,
+      unique:true
     },
     message: {
       type: String
     },
     phone: {
-      type: String, // Store the phone number with country code (e.g., +1234567890)
-      // required: true
+      type: String, 
+      required: true
     },
     countryCode: {
-      type: String, // Store the country code (e.g., +1)
-      // required: true
+      type: String
     },
     countryName: {
-      type: String, // Store the full country name (e.g., United States)
-      // required: true
+      type: String
     },
   },
   { timestamps: true }
